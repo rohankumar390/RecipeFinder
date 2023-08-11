962151a138e9a6a185ad90e8e624e4564b15fcc6
rohankumar390 <87806043+rohankumar390@users.noreply.github.com>
2023-08-11 22:08:52 +0530

    build

--- src/App.js
+++ src/App.js
@@ -4,92 +4,91 @@ import Spinner from "./Components/Spinner";
 import LoadingBar from "react-top-loading-bar";
 import "./App.css";
 const App = () => {
-    const [recipeees, setRecipeees] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [progress, setProgress] = useState(0);
+  const [recipeees, setRecipeees] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
 
-    const [searching, setSearching] = useState("");
-    const [url, setUrl] = useState(
+  const [searching, setSearching] = useState("");
+  const [url, setUrl] = useState(
+    `https://api.edamam.com/api/recipes/v2?type=public&q=${searching}&app_id=8e61d206&app_key=6d2ac43a24b3a3fb87c21d003c9eaaff%09`
+  );
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      setProgress(10);
+      setUrl(
         `https://api.edamam.com/api/recipes/v2?type=public&q=${searching}&app_id=8e61d206&app_key=6d2ac43a24b3a3fb87c21d003c9eaaff%09`
+      );
+      setProgress(30);
+      setProgress(70);
+      setProgress(100);
+    }
+  };
+
+  const submit = (e) => {
+    setProgress(10);
+    setUrl(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${searching}&app_id=8e61d206&app_key=6d2ac43a24b3a3fb87c21d003c9eaaff%09`
     );
+    setProgress(30);
+    setProgress(70);
+    setProgress(100);
+  };
+  const newData = (e) => {
+    setSearching(e.target.value);
+  };
 
-    const handleKeyDown = (event) => {
-        if (event.key === "Enter") {
-            setProgress(10);
-            setUrl(
-                `https://api.edamam.com/api/recipes/v2?type=public&q=${searching}&app_id=8e61d206&app_key=6d2ac43a24b3a3fb87c21d003c9eaaff%09`
-            );
-            setProgress(30);
-            setProgress(70);
-            setProgress(100);
-        }
-    };
+  useEffect(() => {
+    getReci();
+  }, [url]);
 
-    const submit = (e) => {
-        setProgress(10);
-        setUrl(
-            `https://api.edamam.com/api/recipes/v2?type=public&q=${searching}&app_id=8e61d206&app_key=6d2ac43a24b3a3fb87c21d003c9eaaff%09`
-        );
-        setProgress(30);
-        setProgress(70);
-        setProgress(100);
-    };
-    const newData = (e) => {
-        setSearching(e.target.value);
-    };
+  const getReci = async () => {
+    setProgress(100);
+    setProgress(30);
 
-    useEffect(() => {
-        getReci();
-    }, [url]);
+    setLoading(true);
+    const res = await fetch(url);
+    const data = await res.json();
+    setLoading(false);
+    setRecipeees(data.hits);
+    console.log(recipeees);
+    setProgress(70);
+    setProgress(100);
+  };
 
-    const getReci = async () => {
-        setProgress(100);
-        setProgress(30);
+  return (
+    <>
+      <div className="heading">
+        <h1> MY RECIPE </h1>
+        <h3>What Do Want To Eat?</h3>
+      </div>
 
-        setLoading(true);
-        const res = await fetch(url);
-        const data = await res.json();
-        setLoading(false);
-        setRecipeees(data.hits);
-        console.log(recipeees);
-        setProgress(70);
-        setProgress(100);
-    };
+      <LoadingBar height={3} color="#f11946" progress={progress} />
+      {loading && <Spinner />}
 
-    return (
-        <>
-            <div className="heading">
-                <h1> MY RECIPE </h1>
-                <h3>What Do Want To Eat?</h3>
-            </div>
-
-            <LoadingBar height={3} color="#f11946" progress={progress} />
-            {loading && <Spinner />}
-
-            <div className="inp">
-                <input
-                    type="text"
-                    style={{ width: "700px", height: "37px" }}
-                    onChange={newData}
-                    onKeyDown={handleKeyDown}
-                ></input>
-                <button type="submit" className="btn btn-dark" onClick={submit}>
-                    Submit
-                </button>
-            </div>
-            <div className="gridCont">
-                {recipeees.map((r) => (
-                    <RecipeItem
-                        label={r.recipe.label}
-                        calories={r.recipe.calories}
-                        image={r.recipe.image}
-                        ingredients={r.recipe.ingredients}
-                    />
-                ))}
-            </div>
-
-        </>
-    );
+      <div className="inp">
+        <input
+          type="text"
+          style={{ width: "700px", height: "37px" }}  
+          onChange={newData}
+          onKeyDown={handleKeyDown}
+        ></input>
+        <button type="submit" className="btn btn-dark" onClick={submit}>
+          Submit
+        </button>
+      </div>
+      <div className="gridCont">
+        {recipeees.map((r) => (
+          <RecipeItem
+            label={r.recipe.label}
+            calories={r.recipe.calories}
+            image={r.recipe.image}
+            ingredients={r.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default App;
